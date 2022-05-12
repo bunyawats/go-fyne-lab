ac79d3cc0922704d1141dcbcd8aee24238d299ff
Bunyawat Singchai <bunyawat.s@gmail.com>
2022-05-12 14:05:17 +0700

    code from exploring fyne document

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
+	"image/color"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
+func setContentToText(c fyne.Canvas) {
+	green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+	text := canvas.NewText("Text", green)
+	text.TextStyle.Bold = true
+	c.SetContent(text)
+}
 
-	w.SetContent(widget.NewLabel("Hello World!"))
-	w.ShowAndRun()
+func setContentToCircle(c fyne.Canvas) {
+	red := color.NRGBA{R: 0xff, G: 0x33, B: 0x33, A: 0xff}
+	circle := canvas.NewCircle(color.White)
+	circle.StrokeWidth = 4
+	circle.StrokeColor = red
+	c.SetContent(circle)
+}
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Widget")
+
+	myWindow.SetContent(widget.NewEntry())
+	myWindow.ShowAndRun()
 }
